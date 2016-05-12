e2b160968d1ef940e72e62166f57bcdf4ced8990
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-05-12 15:08:57 +0700

    Start filling in missing doc comments.

--- src/parser/nodes.rs
+++ src/parser/nodes.rs
@@ -4,8 +4,6 @@
 //! by the currently permissible set of commands and their
 //! parameters.
 
-#![allow(missing_docs)]
-
 use std::rc::Rc;
 
 /// Minimum priority.
@@ -15,29 +13,42 @@ pub const PRIORITY_PARAMETER: i32 = -10;
 /// The default priority.
 pub const PRIORITY_DEFAULT: i32 = 0;
 
+/// A node in the tree of commands and their parameters
+/// used by the `Parser`.
 pub trait Node {
+    /// The data describing this node.
     fn node_data(&self) -> &NodeFields;
 
+    /// Nodes that are children of this node. Used to
+    /// by the `Parser` during `advance`, `complete`, etc.
     fn successors(&self) -> &Vec<Rc<Node>> {
         &self.node_data().successors
     }
 
+    /// The text used to identify this node in help text.
+    /// This is typically the node name, either in plain
+    /// form or decorated for parameters.
     fn help_symbol(&self) -> String {
         self.node_data().name.to_string()
     }
 
+    /// Help text describing this node.
     fn help_text(&self) -> &String {
         unimplemented!();
     }
 
+    /// Hidden nodes are still found for matching, but are
+    /// hidden from completion.
     fn hidden(&self) -> bool {
         self.node_data().hidden
     }
 
+    /// The name of this node.
     fn name(&self) -> &String {
         &self.node_data().name
     }
 
+    /// This priority of this node during matching and completion.
     fn priority(&self) -> i32 {
         self.node_data().priority
     }
@@ -55,6 +66,7 @@ pub struct NodeFields {
     hidden: bool,
 }
 
+/// A node representing a command.
 pub struct CommandNode {
     fields: CommandNodeFields,
 }
@@ -77,16 +89,23 @@ impl Node for CommandNode {
 }
 
 impl CommandNode {
+    /// The handler which is executed once this node has been accepted.
     pub fn handler(&self) -> Option<fn(&node: Node) -> ()> {
         self.fields.handler
     }
 
-    /// Get the parameter nodes for this command
+    /// Get the parameter nodes for this command.
     pub fn parameters(&self) -> &Vec<Rc<ParameterNode>> {
         &self.fields.parameters
     }
 }
 
+/// A wrapper node wraps another command.
+///
+/// This is used for the help command so that it can complete
+/// normal commands.
+///
+/// The `successors` will be those of the wrapped node.
 pub struct WrapperNode {
     fields: WrapperNodeFields,
 }
@@ -106,23 +125,32 @@ impl Node for WrapperNode {
     }
 }
 
+/// A repeatable node is an internal helper for representing
+/// nodes that can be repeated, like some parameters.
 pub trait RepeatableNode: Node {
+    /// Internal data for a repeatable node.
     fn repeatable_data(&self) -> &RepeatableNodeFields;
 
+    /// Whether or not this node can be repeated. A repeated
+    /// node can be `accept`ed multiple times.
     fn repeatable(&self) -> bool {
         self.repeatable_data().repeatable
     }
 
+    /// If present, this node will no longer be `acceptable`.
     fn repeat_marker(&self) -> &Option<Rc<Node>> {
         &self.repeatable_data().repeat_marker
     }
 }
 
+/// The data for a repeatable node.
 pub struct RepeatableNodeFields {
     repeatable: bool,
     repeat_marker: Option<Rc<Node>>,
 }
 
+/// A node that represented the name portion of a named
+/// parameter.
 pub struct ParameterNameNode {
     fields: ParameterNameNodeFields,
 }
@@ -154,9 +182,13 @@ impl RepeatableNode for ParameterNameNode {
     }
 }
 
+/// Parameter nodes.
 pub trait ParameterNode {
+    /// Internal data for a parameter node.
     fn parameter_data(&self) -> &ParameterNodeFields;
 
+    /// A `required` parameter must be supplied for the
+    /// command line being parsed to be valid.
     fn required(&self) -> bool {
         self.parameter_data().required
     }
@@ -168,6 +200,7 @@ impl RepeatableNode for ParameterNode {
     }
 }
 
+/// Data for parameter nodes.
 pub struct ParameterNodeFields {
     node: NodeFields,
     repeatable: RepeatableNodeFields,
@@ -194,6 +227,10 @@ impl Node for ParameterNode {
     }
 }
 
+/// A flag parameter node.
+///
+/// When implemented, this will only have a value of
+/// true when it is present.
 pub struct FlagParameterNode {
     fields: ParameterNodeFields,
 }
@@ -204,6 +241,7 @@ impl ParameterNode for FlagParameterNode {
     }
 }
 
+/// A named parameter node.
 pub struct NamedParameterNode {
     fields: ParameterNodeFields,
 }
@@ -214,6 +252,8 @@ impl ParameterNode for NamedParameterNode {
     }
 }
 
+/// A simple parameter node. This is only present in a command
+/// line as a value.
 pub struct SimpleParameterNode {
     fields: ParameterNodeFields,
 }
