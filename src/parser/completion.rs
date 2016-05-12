64de1d2e623f1fa27f66efc332353b410e30b9ec
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-05-12 11:14:12 +0700

    Completion: Create CompletionOptions correctly.

--- src/parser/completion.rs
+++ src/parser/completion.rs
@@ -61,12 +61,55 @@ impl<'t> Completion<'t> {
                complete_options: Vec<&str>,
                other_options: Vec<&str>)
                -> Completion<'t> {
+        // Preserve all of the options while still &str so that
+        // we can use this with longest_common_prefix later.
+        let mut all_options = complete_options.clone();
+        all_options.extend(other_options.iter().cloned());
+
+        // Convert to String...
+        let mut complete_options = complete_options.into_iter()
+                                                   .map(|o| o.to_string())
+                                                   .collect::<Vec<_>>();
+        let mut other_options = other_options.into_iter()
+                                             .map(|o| o.to_string())
+                                             .collect::<Vec<_>>();
+        // Apply token restrictions
+        if let Some(t) = token {
+            // Filter options using token.
+            let token_text = t.text.to_string();
+            complete_options.retain(|o| o.starts_with(t.text));
+            other_options.retain(|o| o.starts_with(t.text));
+            // If not exhaustive, then add the current token as
+            // an incomplete option.
+            if !exhaustive && !complete_options.contains(&token_text) &&
+               !other_options.contains(&token_text) {
+                other_options.push(token_text);
+            }
+        }
+        // Add longest common prefix as an incomplete options, but
+        // filter it against the existing options and the token.
+        let lcp = longest_common_prefix(all_options).to_string();
+        if !complete_options.contains(&lcp) && !other_options.contains(&lcp) {
+            match token {
+                Some(t) => {
+                    if lcp != t.text {
+                        other_options.push(lcp)
+                    }
+                }
+                None => other_options.push(lcp),
+            }
+        }
+        // Convert options to CompletionOption.
+        let mut options = complete_options.into_iter()
+                                          .map(|o| CompletionOption::new(o, true))
+                                          .collect::<Vec<_>>();
+        options.extend(other_options.into_iter().map(|o| CompletionOption::new(o, false)));
         Completion {
             help_symbol: help_symbol,
             help_text: help_text,
             token: token,
             exhaustive: exhaustive,
-            options: vec![],
+            options: options,
         }
     }
 }
